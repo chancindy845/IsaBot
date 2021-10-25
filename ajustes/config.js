cd6c212e360cd05c4af5818a53cd52a235ac01ba
tukibdma@gmail.com <91396589+chancindy845@users.noreply.github.com>
2021-10-25 17:07:51 -0500

    Update and rename settings/config.js to ajustes/config.js

--- ajustes/config.js
+++ ajustes/config.js
@@ -1,6 +1,6 @@
 const config = {
-        botName: 'ShanBot',
-        ownerName: 'Shan',
+        botName: 'IsaBot',
+        ownerName: 'Dragón',
         youtube: 'YOUTUBE_LINK',
         instagram: 'INSTAGRAM_LINK',
 }
