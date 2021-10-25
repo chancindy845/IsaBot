83596538b063975fadc3480016001a9a2b29c5b7
tukibdma@gmail.com <91396589+chancindy845@users.noreply.github.com>
2021-10-25 16:56:47 -0500

    Update levelmenu.js

--- src/levelmenu.js
+++ src/levelmenu.js
@@ -26,7 +26,7 @@ PARA VER TU XP EN TIEMPO REAL
     
 *Recuerda que si actualizas tu bot o borras el numero del bot o otra cosa todos los levels de los particiantes quedaran en 0*
     
-_*by shanduy*_
+_*by Dragón*_
 `
 
 }
